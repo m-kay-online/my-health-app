ff0b4dbb5608e76630c18b89a09273fd2b11a5b8
Manoj Kumar <e123.mk@gmail.com>
2025-01-19 20:29:03 +0530

    Background and Sidebar component added to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
@@ -6,22 +6,34 @@ import PatientForm from './components/PatientForm';
 import PatientDetails from './components/PatientDetails';
 import TestMaster from './components/TestMaster';
 import PatientInfo from './components/PatientInfo';
+import Sidebar from './components/Sidebar';
+
+const Main = () => {
+    const location = useLocation();
+    const showSidebar = location.pathname !== '/' && location.pathname !== '/signup';
+
+    return (
+        <div className='min-h-screen bg-gradient-to-r from-violet-600 to-indigo-600 flex'>
+            {showSidebar && <Sidebar />}
+            <div className={showSidebar ? 'ml-64 w-full' : 'w-full'}>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/new-patient" element={<PatientForm onClose={() => window.history.back()} />} />
+                    <Route path="/patient-details" element={<PatientDetails />} />
+                    <Route path="/patient-info/:id" element={<PatientInfo />} />
+                    <Route path="/test-master" element={<TestMaster />} />
+                </Routes>
+            </div>
+        </div>
+    );
+};
 
 const App = () => (
-    <div className='h-screen bg-gradient-to-r from-violet-600 to-indigo-600'>
-        <Router>
-        <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/new-patient" element={<PatientForm onClose={() => window.history.back()} />} />
-            <Route path="/patient-details" element={<PatientDetails />} />
-            <Route path="/patient-info/:id" element={<PatientInfo />} />
-            <Route path="/test-master" element={<TestMaster />} />
-        </Routes>
+    <Router>
+        <Main />
     </Router>
-    </div>
-    
 );
 
 export default App;
